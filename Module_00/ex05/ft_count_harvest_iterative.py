ac615876b07e3024c64d896fc351f1ec410e73be
Hamid Oukaamouch <hamidkaamouch123@gmail.com>
2026-02-09 10:24:50 +0100

    format string + remove extra spaces

--- Module_00/ex05/ft_count_harvest_iterative.py
+++ Module_00/ex05/ft_count_harvest_iterative.py
@@ -1,5 +1,5 @@
 def ft_count_harvest_iterative():
     n = int(input("Days until harvest: "))
     for i in range(n):
-        print("Day ", i+1)
+        print("Day", i+1)
     print("Harvest time!")
--- Module_00/ex05/ft_count_harvest_recursive.py
+++ Module_00/ex05/ft_count_harvest_recursive.py
@@ -1,13 +1,10 @@
 def ft_count_harvest_helper(n):
     if n > 1:
         ft_count_harvest_helper(n - 1)
-    print("Day ", n)
+    print("Day", n)
 
 
 def ft_count_harvest_recursive():
     n = int(input("Days until harvest: "))
     ft_count_harvest_helper(n)
     print("Harvest time!")
-
-
-ft_count_harvest_recursive()
