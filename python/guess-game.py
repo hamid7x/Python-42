b6fb67619ed601a3d9c75b307215298c4f1843e8
Hamid Oukaamouch <hamidkaamouch123@gmail.com>
2026-02-12 17:04:36 +0100

    change names and remove extra functions

--- python/guess-game.py
+++ python/guess-game.py
@@ -8,21 +8,21 @@ def show_attempts(num):
 
 def play():
     while True:
-        guesses = 3
-        program_guess = random.randint(1, 10)
-        show_attempts(guesses)
+        remaining_attempts = 3
+        secret_number = random.randint(1, 10)
+        show_attempts(remaining_attempts)
         user_guess = int(input("I'm thinking of a number (1-10)! Try to guess the number: "))
-        while user_guess != program_guess and guesses != 1:
-            guesses -= 1
-            show_attempts(guesses)
-            if user_guess > program_guess:
+        while user_guess != secret_number and remaining_attempts > 1:
+            remaining_attempts -= 1
+            show_attempts(remaining_attempts)
+            if user_guess > secret_number:
                 user_guess = int(input("Too High! Guess again: "))
-            elif user_guess < program_guess:
+            elif user_guess < secret_number:
                 user_guess = int(input("Too low! Guess again: "))
-        if (user_guess == program_guess):
+        if (user_guess == secret_number):
             print("Congrats! That's it!🎉🥳")
         else:
-            print(f"Yout lose!❌ The number was {program_guess}")
+            print(f"Yout lose!❌ The number was {secret_number}")
         choice = input("Would like to play again (yes/no) ")
         if choice.lower() == 'no':
             print('Thanks for playing!')
