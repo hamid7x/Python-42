3916e99d71f37905b311ed1771e738ad09d9c3bb
Hamid Oukaamouch <hamidkaamouch123@gmail.com>
2026-02-12 18:31:01 +0100

    new feat: add menu, add levels for the gaem

--- python/guess-game.py
+++ python/guess-game.py
@@ -1,10 +1,29 @@
 # let's build a guess number game :
 import random
 
-easy_message = (
+
+max_range = [10, 50, 100]
+message = [(
    "I'm thinking of a number (1-10)! "
    "Try to guess the number: "
-)
+    ),
+    (
+        "I'm thinking of a number (1-50)! "
+    "Try to guess the number: "
+    ),
+    (
+        "I'm thinking of a number (1-100)! "
+        "Try to guess the number: "
+    )
+]
+
+
+def dificilty_level():
+    print('1 -> easy')
+    print('2 -> meduim')
+    print('3 -> hard')
+    n = int(input('Chose ur level: '))
+    return n
 
 
 def show_attempts(num):
@@ -14,11 +33,11 @@ def show_attempts(num):
 def play():
     while True:
         remaining_attempts = 3
-        secret_number = random.randint(1, 10)
+        secret_number = random.randint(1, max_range[level - 1])
         show_attempts(remaining_attempts)
         while True:
             try:
-                user_guess = int(input(easy_message))
+                user_guess = int(input(message[level - 1]))
                 break
             except ValueError:
                 print('Please enter valid number!')
@@ -32,11 +51,27 @@ def play():
         if (user_guess == secret_number):
             print("Congrats! That's it!🎉🥳")
         else:
-            print(f"Yout lose!❌ The number was {secret_number}")
+            print(f"You lose!❌ The number was {secret_number}")
         choice = input("Would like to play again (yes/no) ")
         if choice.lower() == 'no':
             print('Thanks for playing!')
             break
 
 
-play()
+if __name__ == "__main__":
+    while True:
+        level = 1
+        print('='*34)
+        print('|  Welcome to guess number Game  |')
+        print('|  1: start game                 |')
+        print('|  2: dificilty level            |')
+        print('|  3: exit                       |')
+        print('='*34)
+        n = int(input('Enter you choice: '))
+        if n == 1:
+            play(level)
+        elif n == 2:
+            level = dificilty_level()
+        else:
+            print('GoodBye!!')
+            break
