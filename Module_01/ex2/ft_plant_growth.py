f49cf4292f4f797b90ea21651aa170f38a62fc12
Hamid Oukaamouch <hamidkaamouch123@gmail.com>
2026-02-13 14:50:25 +0100

    ft_plant_growth

--- Module_01/ex2/ft_plant_growth.py
+++ Module_01/ex2/ft_plant_growth.py
@@ -1,27 +1,31 @@
-def main():
-    class Plant:
-        def __init__(self, name, height, age):
-            self.name = name
-            self.height = height
-            self._age = age
 
-        def grow(self):
-            self.height += 1
-        
-        def age(self):
-            self._age += 1
-        
-        def get_info(self):
-            print(f"{self.name}: {self.height}cm, {self.age} days old")
+class Plant:
+    growth = 0
 
-    my_plant = Plant('Rose', 25, 30)
-    for i in range(1, 8):
-        print(f"=== Day {i} ===")
-        print(f"{my_plant.name}: {my_plant.height}cm, {my_plant._age} days old")
-        my_plant.grow()
-        my_plant.age()
-    print(f'Growth this week: +{i - 1}cm')
+    def __init__(self, name, height, age):
+        self.name = name
+        self.height = height
+        self._age = age
+
+    def grow(self):
+        Plant.growth += 1
+        self.height += 1
+
+    def age(self):
+        self._age += 1
+
+    def get_info(self):
+        print(f"{self.name}: {self.height}cm, {self.age} days old")
 
 
 if __name__ == "__main__":
-    main()
+    my_plant = Plant('Rose', 25, 30)
+    for i in range(1, 7):
+        print(f"=== Day {i} ===")
+        name = my_plant.name
+        height = my_plant.height
+        age = my_plant._age
+        print(f"{name}: {height}cm, {age} days old")
+        my_plant.grow()
+        my_plant.age()
+    print(f'Growth this week: +{Plant.growth}cm')
