8283062f243b15f370a3590a5b5345f0ddb32036
Hamid Oukaamouch <hamidkaamouch123@gmail.com>
2026-02-15 19:33:36 +0000

    add ft_plant_factory

---
+++ Module_01/ex3/ft_plant_factory.py
@@ -0,0 +1,33 @@
+
+class Plant:
+    counter = 0
+
+    def __init__(self, name, height, age):
+        self.name = name
+        self.height = height
+        self.age = age
+
+    def create_plant(self):
+        name = self.name
+        height = self.height
+        age = self.age
+        print(f"Created: {name} ({height}cm, {age} days)")
+        Plant.counter += 1
+
+
+if __name__ == "__main__":
+    print("=== Plant Factory Output ===")
+    plants_data = [
+        {'name': 'Rose', 'height': 25, 'age': 30},
+        {'name': 'Oak', 'height': 200, 'age': 365},
+        {'name': 'Cactus', 'height': 5, 'age': 90},
+        {'name': 'Sunflower', 'height': 80, 'age': 45},
+        {'name': 'Fern', 'height': 15, 'age': 120}
+    ]
+    for i in range(5):
+        name = plants_data[i]['name']
+        height = plants_data[i]['height']
+        age = plants_data[i]['age']
+        plant = Plant(name, height, age)
+        plant.create_plant()
+    print(f"\nTotal plants created: {Plant.counter}")
