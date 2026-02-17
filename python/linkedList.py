fc9d62ecc7acec36d53856374b77974be45fb890
Hamid Oukaamouch <hamidkaamouch123@gmail.com>
2026-02-17 19:21:25 +0000

    working with single linked list

---
+++ python/linkedList.py
@@ -0,0 +1,42 @@
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert_at_beginning(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def insert_at_end(self, data):
+        new_node = Node(data)
+
+        if not self.head:
+            self.head = new_node
+            return
+        curr = self.head
+        while curr.next:
+            curr = curr.next
+        curr.next = new_node
+
+    def display(self):
+        curr = self.head
+        while curr:
+            print(curr.data, end=" -> ")
+            curr = curr.next
+        print("None")
+
+
+l = LinkedList()
+l.insert_at_beginning(10)
+l.insert_at_beginning(20)
+l.insert_at_beginning(30)
+l.insert_at_end(40)
+
+l.display()
