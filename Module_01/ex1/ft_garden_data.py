3f5df2407794bef90f5c00feb36562a7c9971ef9
Hamid Oukaamouch <hamidkaamouch123@gmail.com>
2026-02-19 17:46:45 +0000

    add type hint

--- Module_01/ex1/ft_garden_data.py
+++ Module_01/ex1/ft_garden_data.py
@@ -1,6 +1,6 @@
 
 class Plant:
-    def __init__(self, name: str, height, age):
+    def __init__(self, name: str, height: int, age: int) -> None:
         self.name = name
         self.height = height
         self.age = age
