e5ba357e3d69ba6b4eee00aa2d3790170feb4f54
Hamid Oukaamouch <hamidkaamouch123@gmail.com>
2026-02-23 21:25:27 +0000

    add ex2

--- Module_01/ex2/ft_plant_growth.py
+++ Module_01/ex2/ft_plant_growth.py
@@ -3,11 +3,9 @@ class Plant:
         self.name = name
         self.height = height
         self._age = age
-        self.growth = 0
 
     def grow(self) -> None:
         self.height += 1
-        self.growth += 1
 
     def age(self) -> None:
         self._age += 1
@@ -17,10 +15,19 @@ class Plant:
 
 
 if __name__ == "__main__":
-    plant = Plant('Rose', 25, 30)
-    for i in range(1, 8):
-        print(f"=== Day {i} ===")
-        plant.get_info()
-        plant.grow()
-        plant.age()
-    print(f"Growth this week: +{plant.growth - 1}cm")
+    plants = [
+        Plant('Rose', 25, 30),
+        Plant('Cactus', 5, 90),
+        Plant('Sunflower', 80, 45)
+    ]
+    print("=== Day 1 ===")
+    for p in plants:
+        p.get_info()
+    for i in range(1, 7):
+        for p in plants:
+            p.grow()
+            p.age()
+    print(f"=== Day {i + 1} ===")
+    for p in plants:
+        p.get_info()
+    print(f"Growth this week: +{i}cm")
