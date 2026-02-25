0aee00650205e868fa0b8ceeee1dd6d5955581f1
Hamid Oukaamouch <hamidkaamouch123@gmail.com>
2026-02-25 17:55:39 +0000

    add ex2

---
+++ Module_02/ex2/ft_custom_errors.py
@@ -0,0 +1,67 @@
+class GardenError(Exception):
+    pass
+
+
+class PlantError(GardenError):
+    pass
+
+
+class WaterError(GardenError):
+    pass
+
+
+def custom_plant_error(name: str, temp: int) -> None:
+    plants = ['carrot', 'tomato', 'potato', 'rose', 'sunflower']
+    if name not in plants:
+        raise PlantError('Invalid plant name!')
+    if temp > 40:
+        raise PlantError(f"the {name} plant is wilting!")
+    if temp < 0:
+        raise PlantError(f"Temperature for {name} is too low!")
+
+
+def custom_water_error(tank_level: int):
+    if tank_level < 0:
+        raise WaterError("Invalid input tank level can't be negative")
+    if tank_level < 40:
+        raise WaterError('Not enough water in the tank!')
+
+
+def test_custom_error():
+    print('Testing PlantError...')
+    try:
+        custom_plant_error('tomato', 70)
+    except PlantError as e:
+        print(f'{e}\n')
+    except Exception as e:
+        print(e)
+
+    print('Testing WaterError...')
+    try:
+        custom_water_error(20)
+    except WaterError as e:
+        print(f'{e}\n')
+    except Exception as e:
+        print(e)
+
+    print('Testing catching all garden errors...')
+    try:
+        custom_plant_error('carrot', 80)
+    except GardenError as e:
+        print(f'Caught a garden error: {e}')
+    except Exception as e:
+        print(e)
+
+    try:
+        custom_water_error(30)
+    except GardenError as e:
+        print(f'Caught a garden error: {e}')
+    except Exception as e:
+        print(e)
+
+    print('\nAll custom error types work correctly!')
+
+
+if __name__ == "__main__":
+    print("=== Custom Garden Errors Demo ===\n")
+    test_custom_error()
