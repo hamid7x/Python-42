7ccf0ac557fc84259a530922354b0cac7c536b59
Hamid Oukaamouch <hamidkaamouch123@gmail.com>
2026-02-25 09:59:55 +0000

    add ex0/ft_first_exception

--- Module_02/ex0/ft_first_exception.py
+++ Module_02/ex0/ft_first_exception.py
@@ -1,31 +1,39 @@
-def check_temperature(temp_str: str) -> None:
-    print("Testing temperature: {temp_str}")
+def check_temperature(temp_str: str) -> int | None:
+    print(f"Testing temperature: {temp_str}")
     try:
         try:
             temp_int = int(temp_str)
         except ValueError:
             print(f"Error: '{temp_str}' is not a valid number")
+            return None
         if temp_int > 40:
-            raise ValueError(f"Error: {temp_int}°C is too hot for plants (max 40°C)")
+            raise ValueError(
+                            f"Error: {temp_int}°C"
+                            f" is too hot for plants (max 40°C)")
         elif temp_int < 0:
-            raise ValueError(f"Error: {temp_int}°C is too cold for plants (min 0°C)")
+            raise ValueError(
+                            f"Error: {temp_int}°C"
+                            f" is too cold for plants (min 0°C)"
+                            )
         else:
-            raise ValueError(f"Temperature {temp_int}°C is perfect for plants!")
+            print(f"Temperature {temp_int}°C is perfect for plants!")
+            return temp_int
     except ValueError as err:
         print(err)
-    except Exception as e:
-        print(f"Error: {e}")
+        return None
 
 
-def test_temperature_input(temp_str: str) -> None:
-    check_temperature('25')
-    check_temperature('abc')
-    check_temperature('100')
-    check_temperature('-5')
-    print()
+def test_temperature_input() -> None:
+    temp_tests = ['25', 'abc', '100', '-50']
+    for temp in temp_tests:
+        try:
+            check_temperature(temp)
+        except Exception as e:
+            print(e)
+        print()
 
 
 if __name__ == "__main__":
     print("=== Garden Temperature Checker ===\n")
-
+    test_temperature_input()
     print("All tests completed - program didn't crash!")
