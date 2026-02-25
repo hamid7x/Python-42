4c71e33f2e2c3bc5ef360ead51ca2a805620d6c8
Hamid Oukaamouch <hamidkaamouch123@gmail.com>
2026-02-25 11:28:07 +0000

    add ex0

--- Module_02/ex0/ft_first_exception.py
+++ Module_02/ex0/ft_first_exception.py
@@ -1,36 +1,31 @@
-def check_temperature(temp_str: str) -> int | None:
+def check_temperature(temp_str: str) -> int:
     print(f"Testing temperature: {temp_str}")
     try:
-        try:
-            temp_int = int(temp_str)
-        except ValueError:
-            print(f"Error: '{temp_str}' is not a valid number")
-            return None
-        if temp_int > 40:
-            raise ValueError(
-                            f"Error: {temp_int}°C"
-                            f" is too hot for plants (max 40°C)")
-        elif temp_int < 0:
-            raise ValueError(
-                            f"Error: {temp_int}°C"
-                            f" is too cold for plants (min 0°C)"
-                            )
-        else:
-            print(f"Temperature {temp_int}°C is perfect for plants!")
-            return temp_int
-    except ValueError as err:
-        print(err)
-        return None
+        temp_int = int(temp_str)
+    except ValueError:
+        raise ValueError(f"Error: '{temp_str}' is not valid number")
+    if temp_int > 40:
+        raise ValueError(
+            f"Error: {temp_int}°C is too hot for plants (max 40°C)")
+    if temp_int < 0:
+        raise ValueError(
+            f"Error: {temp_int}°C is too cold for plants (min 0°C)")
+
+    return temp_int
 
 
 def test_temperature_input() -> None:
-    temp_tests = ['25', 'abc', '100', '-50']
-    for temp in temp_tests:
-        try:
-            check_temperature(temp)
-        except Exception as e:
-            print(e)
-        print()
+    try:
+        temp_tests = ['25', 'abc', '100', '-50']
+        for temp in temp_tests:
+            try:
+                value = check_temperature(temp)
+                print(f"Temperature {value}°C is perfect for plants!")
+            except Exception as e:
+                print(e)
+            print()
+    except Exception as err:
+        print(err)
 
 
 if __name__ == "__main__":
