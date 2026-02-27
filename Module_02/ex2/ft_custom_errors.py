cb1fc699cdc16409c9df3ba1541ffc2c8ec1bc7b
Hamid Oukaamouch <hamidkaamouch123@gmail.com>
2026-02-27 16:21:00 +0000

    add ex2

--- Module_02/ex2/ft_custom_errors.py
+++ Module_02/ex2/ft_custom_errors.py
@@ -1,59 +1,55 @@
 class GardenError(Exception):
-    pass
+    def __init__(self, message: str = 'Garden Error!') -> None:
+        super().__init__(message)
 
 
 class PlantError(GardenError):
-    pass
+    def __init__(self, message: str = 'Plant Error!') -> None:
+        super().__init__(message)
 
 
 class WaterError(GardenError):
-    pass
+    def __init__(self, message: str = 'Water Error!') -> None:
+        super().__init__(message)
 
 
-def custom_plant_errors(name: str, temp: int) -> None:
-    plants = ['carrot', 'tomato', 'potato', 'rose', 'sunflower']
-    if name not in plants:
-        raise PlantError('Invalid plant name!')
-    if temp > 40:
-        raise PlantError(f"the {name} plant is wilting!")
-    if temp < 0:
-        raise PlantError(f"Temperature for {name} is too low!")
+def custom_plant_errors(name: str) -> None:
+    if name:
+        raise PlantError(f"The {name} plant is wilting!")
 
 
 def custom_water_errors(tank_level: int):
-    if tank_level < 0:
-        raise WaterError("Invalid input tank level can't be negative")
-    if tank_level < 40:
+    if tank_level <= 0:
         raise WaterError('Not enough water in the tank!')
 
 
 def test_custom_errors():
     print('Testing PlantError...')
     try:
-        custom_plant_errors('carrot', -70)
+        custom_plant_errors('tomato')
     except PlantError as e:
-        print(f'{e}\n')
+        print(f'Caught PlantError: {e}\n')
     except Exception as e:
         print(e)
 
     print('Testing WaterError...')
     try:
-        custom_water_errors(20)
+        custom_water_errors(-2)
     except WaterError as e:
-        print(f'{e}\n')
+        print(f'Caught WaterError: {e}\n')
     except Exception as e:
         print(e)
 
     print('Testing catching all garden errors...')
     try:
-        custom_plant_errors('tomato', 80)
+        custom_plant_errors('tomato')
     except GardenError as e:
         print(f'Caught a garden error: {e}')
     except Exception as e:
         print(e)
 
     try:
-        custom_water_errors(30)
+        custom_water_errors(0)
     except GardenError as e:
         print(f'Caught a garden error: {e}')
     except Exception as e:
