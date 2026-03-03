705b7cdaf133e96d863da198795c9fa31eb16d75
Hamid Oukaamouch <hamidkaamouch123@gmail.com>
2026-03-03 14:11:05 +0000

    ex1/ft_score_analytics

--- Module_03/ex1/ft_score_analytics.py
+++ Module_03/ex1/ft_score_analytics.py
@@ -1,21 +1,33 @@
 import sys
 
+
+def process_scores(args: list[str]) -> list[int]:
+    if len(args) == 0:
+        raise ValueError('No scores provided. Usage: '
+                         'python3 ft_score_analytics.py <score1> <score2> ...')
+    scores = []
+    for arg in args:
+        try:
+            scores += [int(arg)]
+        except ValueError:
+            print(f"'{arg}' Not valid score ... [skipped]")
+    if not scores:
+        raise ValueError('No valid scores provided.')
+
+    return scores
+
+
 if __name__ == "__main__":
     print('=== Player Score Analytics ===')
-    if len(sys.argv) == 1:
-        print('No scores provided. ', end='')
-        print('Usage: python3 ft_score_analytics.py <score1> <score2> ...')
-    else:
-        scores = []
-        for i in range(1, len(sys.argv)):
-            try:
-                scores.append(int(sys.argv[i]))
-            except Exception:
-                print(f"'{sys.argv[i]}' Not valid score ... [skipped]")
+
+    try:
+        scores = process_scores(sys.argv[1:])
         print(f'Scores processed: {scores}')
         print(f'Total players: {len(scores)}')
         print(f'Total score: {sum(scores)}')
         print(f'Average score: {sum(scores) / len(scores)}')
-        print(f'Hight score: {max(scores)}')
+        print(f'High score: {max(scores)}')
         print(f'Low score: {min(scores)}')
         print(f'Score range: {max(scores) - min(scores)}')
+    except Exception as e:
+        print(e)
