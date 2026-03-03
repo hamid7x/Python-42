e0376b0309bebb9a31397a68243c5cf967642999
Hamid Oukaamouch <hamidkaamouch123@gmail.com>
2026-03-03 13:36:57 +0000

    add ex0/ft_command_quest.py

--- Module_03/ex0/ft_command_quest.py
+++ Module_03/ex0/ft_command_quest.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     total_args = len(sys.argv)
     if total_args == 1:
         print('No arguments provided!')
-        print(f'Progarm name: {sys.argv[0]}')
+        print(f'Program name: {sys.argv[0]}')
     else:
         print(f'Program name: {sys.argv[0]}')
         print(f'Arguments received: {total_args - 1}')
