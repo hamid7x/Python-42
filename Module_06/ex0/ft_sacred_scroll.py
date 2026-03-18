6d08bdb005ecb46f6366e5ec117441213e2e10ef
Hamid Oukaamouch <hamidkaamouch123@gmail.com>
2026-03-18 21:47:17 +0000

    add ex0

---
+++ Module_06/ex0/ft_sacred_scroll.py
@@ -0,0 +1,4 @@
+if __name__ == "__main__":
+    print("=== Sacred Scroll Mastery ===")
+    print('Testing direct module access')
+    print('Testing package_level access (controlled by __init__.py)')
