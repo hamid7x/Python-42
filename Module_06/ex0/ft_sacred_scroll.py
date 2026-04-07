556d28b0032d83420af3b207fe16116691bede48
Hamid Oukaamouch <hamidkaamouch123@gmail.com>
2026-04-07 18:38:31 +0100

    part 1

--- Module_06/ex0/ft_sacred_scroll.py
+++
@@ -1,4 +0,0 @@
-if __name__ == "__main__":
-    print("=== Sacred Scroll Mastery ===")
-    print('Testing direct module access')
-    print('Testing package_level access (controlled by __init__.py)')
