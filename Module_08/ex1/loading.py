8a7cfdcecf5a01265bb5263e07ca67432c6342d4
Hamid Oukaamouch <hamidkaamouch123@gmail.com>
2026-04-10 19:14:37 +0100

    add ex1

--- Module_08/ex1/loading.py
+++ Module_08/ex1/loading.py
@@ -1,11 +1,20 @@
-from importlib import util, metadata
+from importlib import util, metadata, import_module
 import sys
+
+
+API_URL = (
+    "https://api.open-meteo.com/v1/forecast"
+    "?latitude=33.0&longitude=-7.0"
+    "&hourly=temperature_2m"
+    "&past_days=7&forecast_days=0"
+)
+
 
 def is_installed(package_name: str) -> bool:
     return util.find_spec(package_name) is not None
 
 
-def check_dependencies(dependencies: list[dict]) -> list[str]:
+def check_dependencies(dependencies: list[dict[str, str]]) -> list[str]:
     missing = []
     for d in dependencies:
         status = 'OK' if is_installed(d['name']) else 'MISSING'
@@ -18,14 +27,43 @@ def check_dependencies(dependencies: list[dict]) -> list[str]:
     return missing
 
 
-def missing_dependencies(missing: list[str]) -> None:
+def missing_dependencies() -> None:
     print("\nMissing dependencies detected!")
-    print(f"Missing: {', '.join(missing)}")
     print("Install with pip:")
     print("  pip install -r requirements.txt")
-    print("\nInstall with Poetry:")
+    print("Or")
+    print("Install with Poetry:")
     print("  poetry install")
     print("\nThen run this program again.")
+
+
+def fetch_api_data(url: str) -> list:
+    requests = import_module('requests')
+    response = requests.get(url)
+    data = response.json()
+    temps = data['hourly']['temperature_2m']
+    return temps
+
+
+def generate_matrix_data(temps: list) -> list:
+    np = import_module('numpy')
+    return np.array(temps)
+
+
+def analyse_matrix_data(temps):
+    pd = import_module("pandas")
+    df = pd.DataFrame(temps, columns=["temperature"])
+    return df
+
+
+def visualize_matrix_data(df):
+    ptl = import_module("matplotlib.pyplot")
+    ptl.plot(df["temperature"])
+    ptl.title("Matrix Temperature Data")
+    ptl.xlabel("Time")
+    ptl.ylabel("Temperature")
+    ptl.savefig("matrix_analysis.png")
+    print("Visualization saved to matrix_analysis.png")
 
 
 if __name__ == "__main__":
@@ -39,6 +77,9 @@ if __name__ == "__main__":
         ]
     missing = check_dependencies(dependencies)
     if missing:
-        missing_dependencies(missing)
+        missing_dependencies()
         sys.exit(1)
-    print("hala hashas")
+    temps = fetch_api_data(API_URL)
+    temps = generate_matrix_data(temps)
+    df = analyse_matrix_data(temps)
+    visualize_matrix_data(df)
