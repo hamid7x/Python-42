034c1a007c9ac7cac327fd98b0e778dc41d67aa7
Hamid Oukaamouch <hamidkaamouch123@gmail.com>
2026-04-15 14:32:44 +0100

    add ex0

--- Module_08/ex0/construct.py
+++ Module_08/ex0/construct.py
@@ -22,13 +22,13 @@ def show_venv_info() -> None:
 
 
 def show_warning() -> None:
-    print("MATRIX STATUS: You're still plugged in")
+    print("\nMATRIX STATUS: You're still plugged in\n")
     print(f"Current Python: {sys.executable}")
     print("Virtual Environment: None detected\n")
     print("WARNING: You're in the global environment!")
     print("The machines can see everything you install.\n")
     print("To enter the construct, run:")
-    print("python -m venv matrix_env")
+    print("python3 -m virtualenv matrix_env")
     print("source matrix_env/bin/activate # On Unix")
     print("matrix_env\\Scripts\\activate # On Windows\n")
     print("Then run this program again.")
